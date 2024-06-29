8697d68c63c52023dc0d3e7756bbbd43f9d2d2ae
rohans6 <rohan.chitale.6@gmail.com>
2024-06-29 17:21:50 +0100

    Added setup file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages,setup
+
+
+with open('requirements.txt','r') as file_object:
+    libraries=[]
+    while True:
+        l=file_object.readline().replace('\n','')
+        if l!="":
+            libraries.append(l)
+        else:
+            break
+print(libraries)
+
+setup(name='sensor',version='0.0.1',author="Rohan Chitale",author_email="rohan.chitale.6@gmail.com",packages=find_packages(),install_requires=libraries)
