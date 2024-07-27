7a5ded22b3af41a1fac39ce2246caf1a10b24e8a
rohans6 <rohan.chitale.6@gmail.com>
2024-07-27 20:45:06 +0100

    Added environment variabels

--- Sensor/Constant/database.py
+++ Sensor/Constant/database.py
@@ -1,3 +1,6 @@
-url='mongodb+srv://rohanchitale6:lDiVf9LKNN05qVno@cluster0.erodrak.mongodb.net/'
+#url='mongodb+srv://rohanchitale6:lDiVf9LKNN05qVno@cluster0.erodrak.mongodb.net/'
 database='rohan'
 collection_name='sensorfaultprediction'
+
+import os
+url=os.getenv('mongodb_url')
